9514d33b390f08dd0635a29917cd6202fac4caba
Cyano Hao <c@cyano.cn>
2019-10-23 18:28:28 +0800

    improve merge: use Megaminx’s algorithm

---
+++ gc.py
@@ -0,0 +1,118 @@
+kvfilter = lambda d, f: { k: v for k, v in d.items() if f(k, v) }
+
+# tag can be str or list
+def RemoveFeature(table, tag):
+	rm = []
+	for f in table['features']:
+		if f[0:4] in tag:
+			rm.append(f)
+	for f in rm:
+		del table['features'][f]
+
+def NowarRemoveFeatures(asianFont):
+	RemoveFeature(asianFont['GSUB'], [ 'locl', 'pwid', 'fwid', 'hwid', 'twid', 'qwid', 'vert', 'vrt2' ])
+	RemoveFeature(asianFont['GPOS'], [ 'palt', 'halt', 'vert', 'vpal', 'vhal' ])
+
+def FiltFeature(table):
+	visibleLanguages = set()
+	visibleFeatures = set()
+	visibleLookups = set()
+	for lid, lang in table['languages'].items():
+		if not lang:
+			continue
+		visibleLanguages.add(lid)
+		if 'requiredFeature' in lang and lang['requiredFeature'] in table['features']:
+			visibleFeatures.add(lang['requiredFeature'])
+		if 'features' not in lang or not lang['features']:
+			lang['features'] = []
+		for f in lang['features']:
+			if f in table['features'] and table['features'][f]:
+				visibleFeatures.add(f)
+	table['languages'] = kvfilter(table['languages'], lambda k, _: k in visibleLanguages)
+	table['features'] = kvfilter(table['features'], lambda k, _: k in visibleFeatures)
+
+	for _, lutids in table['features'].items():
+		for lutid in lutids:
+			if lutid in table['lookups'] and table['lookups'][lutid]:
+				visibleLookups.add(lutid)
+
+	while True:
+		nA = len(visibleLookups)
+		for lid, lut in table['lookups'].items():
+			if not lut or lid not in visibleLookups:
+				continue
+			if lut['type'] in [ 'gsub_chaining', 'gpos_chaining' ]:
+				for rule in lut['subtables']:
+					for application in rule['apply']:
+						visibleLookups.add(application['lookup'])
+		nK = len(visibleLookups)
+		if nK >= nA:
+			break
+
+	table['lookups'] = kvfilter(table['lookups'], lambda k, _ : k in visibleLookups)
+
+def Mark(lut, obj):
+	if type(obj) == str:
+		lut.add(obj)
+	elif type(obj) == list:
+		for s in obj:
+			Mark(lut, s)
+	elif type(obj) == dict:
+		for k, v in obj.items():
+			Mark(lut, k)
+			Mark(lut, v)
+
+def MarkSubtable(lut, type_, st):
+	if type_ in [ 'gsub_single', 'gsub_multi', 'gsub_alternate' ]:
+		for k, v in st.items():
+			if k in lut:
+				Mark(lut, v)
+	elif type_ == 'gsub_ligature':
+		for sub in st['substitutions']:
+			if all([ g in lut for g in sub['from'] ]):
+				Mark(lut, sub['to'])
+	elif type_ == 'gsub_chaining':
+		pass
+	else:
+		Mark(lut, st)
+
+def MarkFont(font):
+	lut = set()
+	while True:
+		lutn = len(lut)
+		if 'glyph_order' in font:
+			Mark(lut, font['glyph_order'][0])
+		Mark(lut, font['cmap'])
+		Mark(lut, font['cmap_uvs'])
+		if 'GSUB' in font:
+			for _, lookup in font['GSUB']['lookups'].items():
+				if 'subtables' in lookup:
+					for st in lookup['subtables']:
+						MarkSubtable(lut, lookup['type'], st)
+		lutn1 = len(lut)
+		if lutn1 == lutn:
+			break
+	
+	for _, g in font['glyf'].items():
+		if 'references' in g:
+			Mark(lut, g['references'])
+	return lut
+
+def Gc(font):
+	if 'GSUB' in font:
+		FiltFeature(font['GSUB'])
+	if 'GPOS' in font:
+		FiltFeature(font['GPOS'])
+
+	for _ in range(16):
+		lut = MarkFont(font)
+		glyf_ = font['glyf']
+		na = len(glyf_)
+		g1 = {}
+		for g, d in glyf_.items():
+			if g in lut:
+				g1[g] = d
+		nk = len(g1)
+		font['glyf'] = g1
+		if nk >= na:
+			break
--- merge.py
+++ merge.py
@@ -1,3 +1,9 @@
+from libotd.gc import Gc
+
+##
+## simple merger: merge glyphs and ignore all OpenType layout features
+##
+
 def AddRef(n, font, ref):
 	if n in ref:
 		return
@@ -29,11 +35,115 @@ def CopyRef(glyph, a, b):
 				a['glyf'][r['glyph']] = b['glyf'][r['glyph']]
 				CopyRef(a['glyf'][r['glyph']], a, b)
 
-def MergeBelow(base, ext):
-	for (u, n) in ext['cmap'].items():
+def MergeSimple(base, ext):
+	for u, n in ext['cmap'].items():
 		if u not in base['cmap'].keys():
 			base['cmap'][u] = n
 			if n not in base['glyf'].keys():
 				glyph = ext['glyf'][n]
 				base['glyf'][n] = glyph
 				CopyRef(glyph, base, ext)
+
+##
+## Megaminx’s merging algorithm: merge glyphs and keep OpenType layout features
+##
+
+def MergeLayout(base, ext, preferExt = False):
+	for fid, value in ext['features'].items():
+		base['features'][fid] = value
+	for lid, value in ext['lookups'].items():
+		base['lookups'][lid] = value
+	for lid, value in ext['languages'].items():
+		if lid in base['languages']:
+			base['languages'][lid]['features'] += ext['languages'][lid]['features']
+		else:
+			base['languages'][lid] = {
+				'features': base['languages']['DFLT_DFLT']['features'] + ext['languages'][lid]['features']
+			}
+	for lid, value in base['languages'].items():
+		if lid not in ext['languages']:
+			base['languages'][lid]['features'] += ext['languages']['DFLT_DFLT']['features']
+	def l(f, s):
+		lookupOrder = []
+		if 'lookupOrder' in f:
+			lookupOrder += f['lookupOrder']
+		if 'lookupOrder' in s:
+			lookupOrder += s['lookupOrder']
+		return lookupOrder
+	base['lookupOrder'] = l(ext, base) if preferExt else l(base, ext)
+
+def MergeGdef(first, second):
+	def m(k):
+		result = {}
+		if k in second:
+			result.update(second)
+		if k in first:
+			result.update(first)
+	return { k: m(k) for k in [ 'markAttachClassDef', 'glyphClassDef', 'ligCarets' ] }
+
+# base and ext must have diffefent prefixes
+def MergeBelow(base, ext, mergeLayout = True):
+	for k, v in ext['cmap'].items():
+		if k not in base['cmap']:
+			base['cmap'][k] = v
+	if 'cmap_uvs' in ext:
+		if 'cmap_uvs' not in base:
+			base['cmap_uvs'] = {}
+		for k, v in ext['cmap_uvs'].items():
+			if k not in base['cmap_uvs']:
+				base['cmap_uvs'][k] = v
+
+	base['glyf'].update(ext['glyf'])
+
+	if mergeLayout:
+		if 'GSUB' in ext:
+			if 'GSUB' in base:
+				MergeLayout(base['GSUB'], ext['GSUB'])
+			else:
+				base['GSUB'] = ext['GSUB']
+		if 'GPOS' in ext:
+			if 'GPOS' in base:
+				MergeLayout(base['GPOS'], ext['GPOS'])
+			else:
+				base['GPOS'] = ext['GPOS']
+		if 'GDEF' in ext:
+			if 'GDEF' in base:
+				MergeGdef(base['GDEF'], ext['GDEF'])
+			else:
+				base['GDEF'] = ext['GDEF']
+
+	Gc(base)
+
+# base and ext must have diffefent prefixes
+def MergeAbove(base, ext, mergeLayout = True):
+	gid0 = ext['glyph_order'][0]
+	for k, v in ext['cmap'].items():
+		if int(k) > 0x20 and v != gid0:
+			base['cmap'][k] = v
+	if 'cmap_uvs' in ext:
+		if 'cmap_uvs' not in base:
+			base['cmap_uvs'] = {}
+		for k, v in ext['cmap_uvs'].items():
+			if v != gid0:
+				base['cmap_uvs'][k] = v
+
+	base['glyf'].update({ k: v for k, v in ext['glyf'].items() if k != gid0 })
+
+	if mergeLayout:
+		if 'GSUB' in ext:
+			if 'GSUB' in base:
+				MergeLayout(base['GSUB'], ext['GSUB'], preferExt = True)
+			else:
+				base['GSUB'] = ext['GSUB']
+		if 'GPOS' in ext:
+			if 'GPOS' in base:
+				MergeLayout(base['GPOS'], ext['GPOS'], preferExt = True)
+			else:
+				base['GPOS'] = ext['GPOS']
+		if 'GDEF' in ext:
+			if 'GDEF' in base:
+				MergeGdef(ext['GDEF'], base['GDEF'])
+			else:
+				base['GDEF'] = ext['GDEF']
+
+	Gc(base)
